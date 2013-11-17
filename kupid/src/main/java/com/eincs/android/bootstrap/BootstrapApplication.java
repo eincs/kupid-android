c9441132bec0b6b517f81cefb1824d8d17d9da83
James Lee <james@vcnc.co.kr>
2013-11-17 17:04:25 +0900

    Initial Commit

---
+++ kupid/src/main/java/com/eincs/android/bootstrap/BootstrapApplication.java
@@ -0,0 +1,14 @@
+package com.eincs.android.bootstrap;
+
+import android.app.Application;
+
+/**
+ * Application implementation of the application. 
+ */
+public class BootstrapApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
