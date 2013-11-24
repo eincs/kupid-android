94938841aec1cfbae7194c953fb116108c2dc137
James Lee <james@vcnc.co.kr>
2013-11-24 12:35:42 +0900

    add dagger, dependency injection library

--- kupid/src/main/java/com/eincs/android/kupid/KApplication.java
+++ kupid/src/main/java/com/eincs/android/kupid/KApplication.java
@@ -1,5 +1,8 @@
 package com.eincs.android.kupid;
 
+import com.google.common.base.Preconditions;
+
+import dagger.ObjectGraph;
 import android.app.Application;
 
 /**
@@ -7,8 +10,23 @@ import android.app.Application;
  */
 public class KApplication extends Application {
 
+	private static KApplication INSTANCE;
+	private static ObjectGraph OBJECT_GRAPH;
+	
+	public static KApplication getInstance() {
+		Preconditions.checkState(INSTANCE!=null, "KApplication is not created yet.");
+		return INSTANCE;
+	}
+	
+	public static ObjectGraph getObjectGraph() {
+		Preconditions.checkState(OBJECT_GRAPH!=null, "ObjectGraph is not created yet.");
+		return OBJECT_GRAPH;
+	}
+	
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		INSTANCE = this;
 	}
+	
 }
