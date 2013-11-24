01279f98078b50c1ae4937e1ce1c6faafb5a22ff
James Lee <james@vcnc.co.kr>
2013-11-24 12:35:02 +0900

    add eventbus singleton

---
+++ kupid/src/main/java/com/eincs/android/kupid/event/KEventBus.java
@@ -0,0 +1,13 @@
+package com.eincs.android.kupid.event;
+
+import de.greenrobot.event.EventBus;
+
+public final class KEventBus {
+	private KEventBus() {}
+	
+	private static final EventBus DEFAULT_EVENTBUS = new EventBus();
+	
+	public static EventBus getDefaultEventBus() {
+		return DEFAULT_EVENTBUS;
+	}
+}
