03cded1fd177519532ba9643a7c10b6de79dd03c
James Lee <james@vcnc.co.kr>
2013-12-01 21:00:32 +0900

    better settings ui

--- kupid/src/main/java/com/eincs/android/kupid/activity/SettingDetailActivity.java
+++ kupid/src/main/java/com/eincs/android/kupid/activity/SettingDetailActivity.java
@@ -8,22 +8,23 @@ import com.eincs.android.kupid.R;
 
 public class SettingDetailActivity extends SherlockPreferenceActivity {
 
-    @SuppressWarnings("deprecation")
+	@SuppressWarnings("deprecation")
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // 아래와 같이 설정합니다.
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        addPreferencesFromResource(R.xml.preference_detail);
-    }
-    
-    @Override
-   	public boolean onOptionsItemSelected(MenuItem item) {
-   		switch (item.getItemId()) {
-   		case android.R.id.home:
-   			onBackPressed();
-   			break;
-   		}
-   		return true;
-   	}
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// 아래와 같이 설정합니다.
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		addPreferencesFromResource(R.xml.preference_detail);
+		setTitle(R.string.subject_hci);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			onBackPressed();
+			break;
+		}
+		return true;
+	}
 }
